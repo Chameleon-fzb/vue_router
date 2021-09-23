5bea8b89c0d066ce28c78d575570dae0ff7585bf
chameleon-fzb <1466518303@qq.com>
2021-09-23 22:06:16 +0800

    路由携带params

---
+++ 4.src路由携带params/App.vue
@@ -0,0 +1,64 @@
+<template>
+	<div>
+		<Header />
+		<div class="container">
+			<div class="row">
+				<!-- Nav tabs -->
+				<ul class="nav nav-stacked col-md-6" role="tablist">
+					<li role="presentation" active-class="active">
+						<!-- vue中使用router-link实现路径改变 -->
+						<router-link
+							to="/home"
+							aria-controls="home"
+							role="tab"
+							data-toggle="tab"
+						>
+							Home
+						</router-link>
+					</li>
+					<li role="presentation" active-class="active">
+						<router-link
+							to="/about"
+							aria-controls="about"
+							role="tab"
+							data-toggle="tab"
+						>
+							About
+						</router-link>
+					</li>
+				</ul>
+				<!-- Tab panes -->
+				<div class="tab-content col-md-6">
+					<router-view></router-view>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+import Header from './components/Header.vue'
+// import Home from './pages/Home.vue'
+// import About from './pages/About.vue'
+export default {
+	name: 'App',
+	components: {
+		Header
+		// Home,
+		// About
+	}
+}
+</script>
+
+<style>
+.list-group-item {
+	position: relative;
+}
+
+.btn {
+	width: 80px;
+	height: 30px;
+	position: absolute;
+	right: 0;
+	bottom: 5px;
+}
+</style>
