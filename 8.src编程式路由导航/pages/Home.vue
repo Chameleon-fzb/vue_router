e7e696015078c2af017d225ea9b70115034e28d4
chameleon-fzb <1466518303@qq.com>
2021-09-24 15:54:12 +0800

    编程式路由导航

---
+++ 8.src编程式路由导航/pages/Home.vue
@@ -0,0 +1,28 @@
+<template>
+	<div role="tabpanel" class="tab-pane active" id="home">
+		<!-- Nav tabs -->
+		<h2>Home组件内容</h2>
+		<ul class="nav nav-tabs" role="tablist">
+			<li>
+				<router-link :to="{ name: 'message' }" replace>
+					Message
+				</router-link>
+			</li>
+			<li>
+				<router-link :to="{ name: 'news' }" replace>
+					News
+				</router-link>
+			</li>
+		</ul>
+		<!-- Tab panes -->
+		<div>
+			<router-view></router-view>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Home'
+}
+</script>
